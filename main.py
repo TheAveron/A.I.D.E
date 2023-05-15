d683b0b97784f6899d352ef2a476ccb22e3a1d86
TheAveron <77922089+TheAveron@users.noreply.github.com>
2023-05-15 15:49:31 +0000

    Feat :sparkles: : Creating runing environnement

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import interactions
-import json
+import os
 
+from keepalive import keep_alive
+
+token = os.environ['TOKEN']
+'''
 with open("configuration.json",'r',encoding='utf8') as config:
     data = json.load(config)
     token=data['token']
     owner=data['owner']
+'''
 
-bot=interactions.Client(token=token)
+bot = interactions.Client(token=token)
 bot.load_extension("Commands.new_users")
 
-@interactions.slash_command(
-	name="stop",
-	description="Stop the bot",
-	scopes=[784712879831646208],
-)
-@interactions.check(
-	interactions.is_owner()
-)
-async def stop(ctx):
-	await ctx.send('Bot stopped !')
-	await bot.stop()
 
+@interactions.slash_command(
+  name="stop",
+  description="Stop the bot",
+  scopes=[784712879831646208],
+)
+@interactions.check(interactions.is_owner())
+async def stop(ctx):
+  await ctx.send('Bot stopped !')
+  await bot.stop()
+
+
+@interactions.listen(event_name="Ready")
+async def on_ready(event: interactions.api.events.Ready):
+  """Envoie ready quand le bot est lancé"""
+  print("Ready !")
+
+keep_alive()
 bot.start()
