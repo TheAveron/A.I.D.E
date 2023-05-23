1156dc08cf2afe2637218e40580eb0fe36831f51
Averon <77922089+TheAveron@users.noreply.github.com>
2023-05-23 18:55:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import interactions
-from interactions import is_owner
-#import os
-import json
-from Utilities.checks import Is_Owner
+import os
 
+from Utilities.checks import Is_Owner
 from keepalive import keep_alive
 
-with open('configuration.json',"r",encoding='utf8') as f:
-	token = json.load(f)['TOKEN']
+token = os.environ['TOKEN']
 
 bot = interactions.Client(token=token, owner_ids=[762729436268593183])
 bot.load_extension("Commands.users")
@@ -19,17 +16,13 @@ bot.load_extension("Commands.users")
 )
 @interactions.check(Is_Owner)
 async def stopbot(ctx:interactions.SlashContext):
-  print(ctx.bot.owner_ids)
   await ctx.send('Bot stopped !')
   await ctx.bot.stop()
-
-  print("STOPED")
 
 @interactions.listen(event_name="Ready")
 async def on_ready(event: interactions.api.events.Ready):
 	"""Envoie ready quand le bot est lancé"""
 	print("Ready !")
-
 
 keep_alive()
 bot.start()
