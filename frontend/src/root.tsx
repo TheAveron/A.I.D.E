6e6f1be9ad04b227afac4460faebdd7caece1516
TheAveron <77922089+TheAveron@users.noreply.github.com>
2025-06-15 01:11:21 +0200

    feat: bringing  to head the tags from the old website

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -15,11 +15,15 @@ export function Layout({
         <html lang="fr" data-theme="light" >
             <head>
                 <meta charSet="UTF-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
-                <title>My App</title>
+                <link rel="icon" type="image/png" href="/images/CC_logo.png" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="stylesheet" type="text/css"
+                    href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.9.0/css/ol.css"
+                    integrity="sha256-jckPZk66EJrEBQXnJ5QC2bD+GxWPDRVVoMGr5vrMZvM=" crossOrigin="anonymous" />
+
+                <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.9.0/build/ol.js"
+                    integrity="sha256-77dogUPZ1WVoK9BDF0CxsKnAouX3YzK6n4tIcbDgtFI=" crossOrigin="anonymous"></script>
+                <title>Guide de CubeCrusader</title>
                 <Meta />
                 <Links />
             </head>
@@ -28,7 +32,7 @@ export function Layout({
                 <ScrollRestoration />
                 <Scripts />
             </body>
-        </html>
+        </html >
     );
 }
 
